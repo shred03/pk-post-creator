1785d70ea9e20cf005d6d62b32cb5ef9835f438d
shred03 <mithunyada12345@gmail.com>
2025-03-12 17:40:34 +0530

    removed year from series filename

--- main.py
+++ main.py
@@ -768,7 +768,7 @@ async def process_title_selection(callback_query: CallbackQuery, tmdb_id: str, m
                 'movie_p': title_data.get('name', 'N/A'),
                 'year_p': title_data.get('first_air_date', 'N/A')[:4] if title_data.get('first_air_date') else 'N/A',
             }
-            additional_message = f"""`[PirecyKings3] S{{season}} E{{episode}} {series_data['movie_p']} ({series_data['year_p']}) @pirecykings3`
+            additional_message = f"""`[PirecyKings3] S{{season}} E{{episode}} {series_data['movie_p']} @pirecykings3`
 
 `S01 English - Hindi [480p]`
 
